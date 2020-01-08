376441b0dc75b6a215192e65f1cf73c4be784df8
rbhog <rbhogavilli@gmail.com>
2020-01-08 17:22:08 -0500

    Fix styling

--- src/main/kotlin/com/team4099/robot2020/subsystems/Intake.kt
+++ src/main/kotlin/com/team4099/robot2020/subsystems/Intake.kt
@@ -1,19 +1,15 @@
 package com.team4099.robot2020.subsystems
 
-import com.ctre.phoenix.motorcontrol.NeutralMode
-import com.revrobotics.SparkMax
-import com.team4099.lib.loop.Loop
-import com.team4099.lib.motorcontroller.GenericSmartMotorController
-import com.team4099.lib.motorcontroller.LazySparkMax
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel.MotorType
 import com.team4099.lib.motorcontroller.SparkMaxControllerFactory
 import com.team4099.lib.subsystem.Subsystem
 import com.team4099.robot2020.config.Constants
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 
-object Intake: Subsystem {
+object Intake : Subsystem {
 
-    //private val sparkmax = SparkMax(Constants.Intake.SPARKMAX.ID)
-    private val sparkMax: LazySparkMax
+    private val sparkMax = CANSparkMax(Constants.Intake.INTAKE_SPARK_MAX_ID, MotorType.kBrushless)
 
     var intakeState = IntakeState.IDLE
 
@@ -26,8 +22,6 @@ object Intake: Subsystem {
     }
 
     init {
-        sparkMax = SparkMaxControllerFactory.createDefaultSparkMax(Constants.Intake.INTAKE_SPARK_MAX_ID)
-
         sparkMax.inverted = false
     }
 
@@ -37,29 +31,19 @@ object Intake: Subsystem {
         sparkMax.set(power)
     }
 
-    override fun outputTelemetry() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun outputTelemetry() { }
 
-    override fun checkSystem() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun checkSystem() { }
 
-    override fun registerLogging() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun registerLogging() { }
 
-    override fun zeroSensors() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun zeroSensors() { }
 
     override fun onStart(timestamp: Double) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         intakeState = IntakeState.IDLE
     }
 
     override fun onLoop(timestamp: Double, dt: Double) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         synchronized(this@Intake) {
             when (intakeState) {
                 IntakeState.IN -> setIntakePower(-1.0)
@@ -72,7 +56,6 @@ object Intake: Subsystem {
     override fun onStop(timestamp: Double) {
         intakeState = IntakeState.IDLE
         setIntakePower(0.0)
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
 //        val loop = object: Loop {
