3a8ff861c5058cd55ccbdb8c018d55dee31b563c
rbhog <rbhogavilli@gmail.com>
2020-01-08 20:37:58 -0500

    Address pull request comments

--- src/main/kotlin/com/team4099/robot2020/config/Constants.kt
+++ src/main/kotlin/com/team4099/robot2020/config/Constants.kt
@@ -177,5 +177,8 @@ object Constants {
         const val FEEDER_IN_SLAVE_VICTOR_ID = 12
 
         const val FEEDER_OUT_TALON_ID = 13
+
+        const val FEEDER_MAX_POWER = 1.0
+        const val FEEDER_HOLD_POWER = 0.5
     }
 }
--- src/main/kotlin/com/team4099/robot2020/subsystems/Feeder.kt
+++ src/main/kotlin/com/team4099/robot2020/subsystems/Feeder.kt
@@ -1,28 +1,27 @@
 package com.team4099.robot2020.subsystems
 
 import com.ctre.phoenix.motorcontrol.ControlMode
-import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import com.team4099.lib.motorcontroller.CTREMotorControllerFactory
 import com.team4099.lib.subsystem.Subsystem
 import com.team4099.robot2020.config.Constants
 
 object Feeder : Subsystem {
-    private val inMasterTalon: TalonSRX
+    private val inMasterTalon = CTREMotorControllerFactory.createDefaultTalon(Constants.Feeder.FEEDER_IN_MASTER_TALON_ID)
     private val inSlaveVictor = CTREMotorControllerFactory.createPermanentSlaveVictor(
             Constants.Feeder.FEEDER_IN_MASTER_TALON_ID,
             Constants.Feeder.FEEDER_IN_SLAVE_VICTOR_ID
     )
 
-    private val outTalon: TalonSRX
+    private val outTalon = CTREMotorControllerFactory.createDefaultTalon(Constants.Feeder.FEEDER_OUT_TALON_ID)
 
     var feederState = FeederState.HOLD
     private var feederInPower = 0.0
+        set(value) {
+            inMasterTalon.set(ControlMode.PercentOutput, value)
+        }
     private var feederOutPower = 0.0
 
     init {
-        inMasterTalon = CTREMotorControllerFactory.createDefaultTalon(Constants.Feeder.FEEDER_IN_MASTER_TALON_ID)
-
-        outTalon = CTREMotorControllerFactory.createDefaultTalon(Constants.Feeder.FEEDER_OUT_TALON_ID)
 
         inMasterTalon.inverted = false
         inSlaveVictor.inverted = true
@@ -32,10 +31,6 @@ object Feeder : Subsystem {
 
     enum class FeederState {
         HOLD, INTAKE, SHOOT
-    }
-
-    private fun setFeederInPower(power: Double) {
-        inMasterTalon.set(ControlMode.PercentOutput, power)
     }
 
     private fun setFeederOutPower(power: Double) {
@@ -54,20 +49,21 @@ object Feeder : Subsystem {
         feederState = FeederState.HOLD
     }
 
+    @Synchronized
     override fun onLoop(timestamp: Double, dT: Double) {
-        synchronized(this@Feeder) {
+        synchronized(this) {
             when (feederState) {
                 FeederState.INTAKE -> {
-                    setFeederOutPower(-0.5)
-                    setFeederInPower(1.0)
+                    feederOutPower = -Constants.Feeder.FEEDER_HOLD_POWER
+                    feederInPower = Constants.Feeder.FEEDER_MAX_POWER
                 }
                 FeederState.HOLD -> {
-                    setFeederOutPower(-0.5)
-                    setFeederInPower(0.25)
+                    feederOutPower = -Constants.Feeder.FEEDER_HOLD_POWER
+                    feederInPower = Constants.Feeder.FEEDER_HOLD_POWER
                 }
                 FeederState.SHOOT -> {
-                    setFeederOutPower(1.0)
-                    setFeederInPower(1.0)
+                    feederOutPower = Constants.Feeder.FEEDER_MAX_POWER
+                    feederInPower = Constants.Feeder.FEEDER_MAX_POWER
                 }
             }
         }
@@ -75,7 +71,7 @@ object Feeder : Subsystem {
 
     override fun onStop(timestamp: Double) {
         feederState = FeederState.HOLD
-        setFeederInPower(0.0)
-        setFeederOutPower(0.0)
+        feederInPower = 0.0
+        feederOutPower = 0.0
     }
 }
