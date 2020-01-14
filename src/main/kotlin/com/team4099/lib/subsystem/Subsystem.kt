af5f836fdb22d4a21496ec77458164ea95104b5c
Aidan Goettsch <aidangoettsch@gmail.com>
2020-01-14 09:51:23 -0500

    make subsystem an interface again, change to declaration site variance instead of usage variance

--- src/main/kotlin/com/team4099/lib/subsystem/Subsystem.kt
+++ src/main/kotlin/com/team4099/lib/subsystem/Subsystem.kt
@@ -6,29 +6,29 @@ import com.team4099.lib.config.Configurable
 /**
  * Represents a subsystem of the robot.
  */
-abstract class Subsystem : Loop {
+interface Subsystem : Loop {
     /**
      * A list of objects that can be configured over SmartDashboard.
      */
-    open val configurableProperties = listOf<Configurable<out Number>>()
+    val configurableProperties: List<Configurable<Number>>
 
     /**
      * Output telemetry from this subsystem. Typically will be data sent to SmartDashboard.
      */
-    abstract fun outputTelemetry()
+    fun outputTelemetry()
 
     /**
      * Verify the function of the subsystem. Should be called when the robot is in test mode.
      */
-    abstract fun checkSystem()
+    fun checkSystem()
 
     /**
      * Register logging parameters for this subsystem.
      */
-    abstract fun registerLogging()
+    fun registerLogging()
 
     /**
      * Reset the subsystem's sensors to a zero state.
      */
-    abstract fun zeroSensors()
+    fun zeroSensors()
 }
