0b762e25511a6f82eeefc8595f0a84dee16487ac
rbhog <rbhogavilli@gmail.com>
2020-01-15 08:06:31 -0500

    Change climber constructor

--- src/main/kotlin/com/team4099/robot2020/subsystems/Climber.kt
+++ src/main/kotlin/com/team4099/robot2020/subsystems/Climber.kt
@@ -1,20 +1,18 @@
 package com.team4099.robot2020.subsystems
 
 import com.ctre.phoenix.motorcontrol.NeutralMode
+import com.team4099.lib.hardware.CTREServoMotorHardware
+import com.team4099.lib.hardware.ServoMotorHardware
 import com.team4099.lib.motorcontroller.CTREMotorControllerFactory
 import com.team4099.lib.subsystem.ServoMotorSubsystem
 import com.team4099.robot2020.config.Constants
 
 object Climber : ServoMotorSubsystem(
     Constants.Climber,
-    CTREMotorControllerFactory.createTalonSRX(
+    CTREServoMotorHardware(
         Constants.Climber.MASTER_ID,
-        CTREMotorControllerFactory.Configuration().apply { neutralMode = NeutralMode.Brake }
-    ),
-    listOf(
-        CTREMotorControllerFactory.createPermanentSlaveVictorSPX(
-            Constants.Climber.SLAVE_ID,
-            Constants.Climber.MASTER_ID
+        listOf(
+            Constants.Climber.SLAVE_ID
         )
     )
 ) {
