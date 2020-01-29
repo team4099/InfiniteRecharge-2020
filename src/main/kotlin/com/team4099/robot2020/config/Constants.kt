206760342e2b1fef8fbb9a210c249cac51f6aa80
kstar67453 <47035194+kstar67453@users.noreply.github.com>
2020-01-29 17:16:21 -0500

    Remove beam break from intake and add second one to feeder

--- src/main/kotlin/com/team4099/robot2020/config/Constants.kt
+++ src/main/kotlin/com/team4099/robot2020/config/Constants.kt
@@ -208,11 +208,11 @@ object Constants {
     }
 
     object BeamBreak {
-        const val INTAKE_BEAM_BREAK_PORT = 0
-        const val FEEDER_BEAM_BREAK_PORT = 0
+        const val IN_BEAM_BREAK_PORT = 0
+        const val OUT_BEAM_BREAK_PORT = 0
 
         // test how long it takes one ball to pass through intake and feeder
-        const val INTAKE_BEAM_BROKEN_BALL_TIME = 0.0
-        const val FEEDER_BEAM_BROKEN_BALL_TIME = 0.0
+        const val IN_BEAM_BROKEN_BALL_TIME = 0.0
+        const val OUT_BEAM_BROKEN_BALL_TIME = 0.0
     }
 }
