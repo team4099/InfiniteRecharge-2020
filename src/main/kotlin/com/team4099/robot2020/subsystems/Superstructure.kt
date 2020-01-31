2a82aa0c205c9d454f9f8175b1203b774dc652cb
Varun <30060599+flash76@users.noreply.github.com>
2020-01-31 18:09:30 -0500

    Set systems to idle in onStart of Superstructure

--- src/main/kotlin/com/team4099/robot2020/subsystems/Superstructure.kt
+++ src/main/kotlin/com/team4099/robot2020/subsystems/Superstructure.kt
@@ -2,10 +2,12 @@ package com.team4099.robot2020.subsystems
 
 import Shooter
 import com.team4099.lib.drive.DriveSignal
+import com.team4099.lib.logging.HelixLogger
 import com.team4099.lib.loop.Loop
 import com.team4099.robot2020.config.Constants.Superstructure
 import com.team4099.robot2020.config.Constants
 import com.team4099.robot2020.statemachines.ShooterStatemachine
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 
 
 object Superstructure : Loop {
@@ -13,11 +15,17 @@ object Superstructure : Loop {
     private var hasStateChanged = false
 
     private var currentWantedState = Superstructure.WantedState.DEFAULT
-    private var currentRobotState = Superstructure.RobotState.DEFAULT
+        set(value) {
+            if (value != currentWantedState) {
+                field = value
+            }
+        }
 
-    override fun onStart(timestamp: Double) {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
-    }
+    private var currentRobotState = Superstructure.RobotState.DEFAULT
+        set(value) {
+            SmartDashboard.putString("RobotState", value.toString())
+            field = value
+        }
 
     override fun onLoop(timestamp: Double, dT: Double) {
         when (currentWantedState) {
@@ -25,6 +33,7 @@ object Superstructure : Loop {
                 Drive.setOpenLoop(DriveSignal.NEUTRAL)
                 Intake.intakeState = Intake.IntakeState.IDLE
                 Climber.positionSetpoint = Constants.Climber.ClimberPosition.DOWN
+                Shooter.shooterState = Shooter.State.IDLE
 
                 currentRobotState = Superstructure.RobotState.IDLING
             }
@@ -36,17 +45,18 @@ object Superstructure : Loop {
                 }
                 currentRobotState = Superstructure.RobotState.SHOOTING
             }
-            Superstructure.WantedState.CLIMBER_CLIMB -> {
-                Climber.positionSetpoint = Constants.Climber.ClimberPosition.UP
-
-                currentRobotState = Superstructure.RobotState.CLIMBING
-            }
             Superstructure.WantedState.INTAKE -> {
                 Feeder.feederState = Feeder.FeederState.INTAKE
                 Intake.intakeState = Intake.IntakeState.IN
 
                 currentRobotState = Superstructure.RobotState.INTAKING
             }
+            Superstructure.WantedState.CLIMBER_CLIMB -> {
+                Climber.positionSetpoint = Constants.Climber.ClimberPosition.UP
+
+                currentRobotState = Superstructure.RobotState.CLIMBING
+            }
+
             Superstructure.WantedState.EXHAUST -> {
                 Shooter.shooterState = Shooter.State.EXHAUST
                 Feeder.feederState = Feeder.FeederState.EXHAUST
@@ -55,6 +65,16 @@ object Superstructure : Loop {
                 currentRobotState = Superstructure.RobotState.EXHAUSTING
             }
         }
+    }
+
+    override fun onStart(timestamp: Double) {
+        Drive.setOpenLoop(DriveSignal.NEUTRAL)
+        Intake.intakeState = Intake.IntakeState.IDLE
+        Climber.positionSetpoint = Constants.Climber.ClimberPosition.DOWN
+        Shooter.shooterState = Shooter.State.IDLE
+        Feeder.feederState = Feeder.FeederState.IDLE
+
+        currentRobotState = Superstructure.RobotState.IDLING
     }
 
     override fun onStop(timestamp: Double) {}
