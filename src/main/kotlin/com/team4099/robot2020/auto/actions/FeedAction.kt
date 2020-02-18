1861736d6715e48d1e3c3db6f26ac510cadf3b7a
Varun <30060599+flash76@users.noreply.github.com>
2020-02-18 16:10:10 -0500

    Add feeder states for FeedAction

--- src/main/kotlin/com/team4099/robot2020/auto/actions/FeedAction.kt
+++ src/main/kotlin/com/team4099/robot2020/auto/actions/FeedAction.kt
@@ -2,10 +2,13 @@ package com.team4099.robot2020.auto.actions;
 
 import com.team4099.lib.auto.Action
 import com.team4099.robot2020.config.Constants
+import com.team4099.robot2020.subsystems.Feeder
+import com.team4099.robot2020.subsystems.Intake
 import com.team4099.robot2020.subsystems.Wrist
 
 class FeedAction : Action {
     override fun isFinished(timestamp: Double): Boolean {
+//        return Feeder.inBeamBroken && Feeder.outBeamBroken
         return true
     }
 
@@ -14,7 +17,8 @@ class FeedAction : Action {
     }
 
     override fun onLoop(timestamp: Double, dT: Double) {
-
+        Intake.intakeState = Intake.IntakeState.IN
+        Feeder.feederState = Feeder.FeederState.HOLD
     }
 
     override fun onStop(timestamp: Double) {
