9048d7ef376fcadc6caf211ddc5b1549fbdf939a
Aidan Goettsch <aidangoettsch@gmail.com>
2020-02-25 16:01:56 -0500

    navx back to spi

--- src/main/kotlin/com/team4099/robot2020/subsystems/Drive.kt
+++ src/main/kotlin/com/team4099/robot2020/subsystems/Drive.kt
@@ -27,8 +27,8 @@ import com.team4099.lib.drive.DriveSignal
 import com.team4099.lib.motorcontroller.CTREMotorControllerFactory
 import com.team4099.lib.subsystem.Subsystem
 import com.team4099.robot2020.config.Constants
-import edu.wpi.first.wpilibj.I2C
-import edu.wpi.first.wpilibj.SerialPort
+import edu.wpi.first.wpilibj.SPI
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 
 object Drive : Subsystem {
     private val rightMasterTalon: TalonFX
@@ -37,7 +37,7 @@ object Drive : Subsystem {
     private val leftMasterTalon: TalonFX
     private val leftSlaveTalon: TalonFX
 
-    private val ahrs = AHRS(SerialPort.Port.kMXP)
+    private val ahrs = AHRS(SPI.Port.kMXP)
     private var trajDuration = 0.0
     private var trajCurTime = 0.0
     private var trajStartTime = 0.0
